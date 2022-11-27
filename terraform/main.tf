fd98717189c6845a5c60e1ffef2b1f8021b2136b
v-polishchuk <st.kreol@gmail.com>
2022-11-27 18:04:16 +0200

    Reformat code with best practices

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,91 +1,91 @@
-variable "aws_region" {
-  default = "us-west-2"
-}
-variable "cidr" {
-  default = "10.10.0.0/16"
-}
-variable "cidr_public" {
-  default = "10.10.10.0/24"
-}
-variable "cidr_private" {
-  default = "10.10.20.0/24"
-}
-variable "tags" {
-  type = map(any)
-  default = {
-    Team        = "hillel devops"
-    Project     = "vpc"
-    Environment = "dev"
-  }
-}
-
-# VPC
-resource "aws_vpc" "default_vpc" {
+# VPC and subnets
+resource "aws_vpc" "this" {
   cidr_block = var.cidr
   tags       = var.tags
 }
 resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.default_vpc.id
+  vpc_id                  = aws_vpc.this.id
   cidr_block              = var.cidr_public
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public"
+  }
 }
 resource "aws_subnet" "private" {
-  vpc_id     = aws_vpc.default_vpc.id
+  vpc_id     = aws_vpc.this.id
   cidr_block = var.cidr_private
+
+  tags = {
+    Name = "private"
+  }
 }
 
-# Internet Gateway
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.default_vpc.id
+# Internet and NAT gateways
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
 }
-resource "aws_eip" "ip_pub" {
+resource "aws_eip" "this" {
   vpc        = true
-  depends_on = [aws_internet_gateway.igw]
+
+  depends_on = [aws_internet_gateway.this]
 }
 resource "aws_nat_gateway" "default" {
-  allocation_id = aws_eip.ip_pub.id
+  allocation_id = aws_eip.this.id
   subnet_id     = aws_subnet.public.id
-  depends_on    = [aws_eip.ip_pub]
+
+  depends_on    = [aws_eip.this]
 }
 
 # Route Table for Public Network
-resource "aws_route_table" "rt_public" {
-  vpc_id = aws_vpc.default_vpc.id
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "public"
+  }
+
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = aws_internet_gateway.this.id
   }
 }
-resource "aws_route_table_association" "link_public" {
+resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.rt_public.id
+  route_table_id = aws_route_table.public.id
 }
 
 # Route Table for Private Network
-resource "aws_route_table" "rt_private" {
-  vpc_id = aws_vpc.default_vpc.id
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "private"
+  }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id         = aws_nat_gateway.default.id
+  }
 }
-resource "aws_route" "route_private" {
-  route_table_id         = aws_route_table.rt_private.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.default.id
-}
-resource "aws_route_table_association" "link_private" {
+resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.rt_private.id
+  route_table_id = aws_route_table.private.id
 }
 
 # Create security groups
-resource "aws_security_group" "sg_public" {
-  vpc_id      = aws_vpc.default_vpc.id
+resource "aws_security_group" "public" {
+  vpc_id      = aws_vpc.this.id
   description = "Allow SSH inbound traffic"
   name        = "public"
+
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -93,20 +93,22 @@ resource "aws_security_group" "sg_public" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-resource "aws_security_group" "sg_private" {
-  vpc_id      = aws_vpc.default_vpc.id
+resource "aws_security_group" "private" {
+  vpc_id      = aws_vpc.this.id
   description = "Allow inbound traffic from public subnet"
-}
-resource "aws_security_group_rule" "rule_all" {
-  security_group_id        = aws_security_group.sg_private.id
-  type                     = "ingress"
-  from_port                = 0
-  to_port                  = 0
-  protocol                 = "-1"
-  source_security_group_id = aws_security_group.sg_public.id
-}
+  name        = "private"
 
-# Create provider
-provider "aws" {
-  region = var.aws_region
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    security_groups = [aws_security_group.public.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
